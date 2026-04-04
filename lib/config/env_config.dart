05a0c541225636cd20dafc3e960578ff5fb0a08b
Choman <264514319+chomandb1-max@users.noreply.github.com>
2026-04-03 21:39:39 -0400

    Remove sensitive config from tracking

--- lib/config/env_config.dart
+++
@@ -1,7 +0,0 @@
-class AppConfig {
-  static const String supabaseUrl = "https://fljchnkqhaopmlexsuru.supabase.co";
-  static const String supabaseAnonKey = "sb_publishable_oKnrwnECmxcYk5YtHhQK-Q_bsbgZfci";
-  
-  static const String adminUser = "smart";
-  static const String adminPass = "0909";
-}
